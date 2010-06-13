81cfb43f76041aa8a6ef29e2e812ba7ffd13feb0
Chris Smith <csmith32@gmail.com>
2010-06-13 16:28:07 -0400

    Stripped current plan architecture to prepare for expression-rewrite-compile approach.
    Did not use branch because the new arch will be the basis for any old arch ideas that are incorporated.

--- EseLinq/Provider.cs
+++ EseLinq/Provider.cs
@@ -5,7 +5,6 @@ using System.Linq.Expressions;
 using System.Text;
 
 using EseObjects;
-using EseLinq.Plans;
 
 namespace EseLinq
 {
@@ -20,26 +19,7 @@ namespace EseLinq
 
 		public IQueryable<T> CreateQuery<T>(Expression exp)
 		{
-			Translator.Downstream downs = new Translator.Downstream();
-			downs.Init();
-			downs.type_map = Storage.Flat<int>.StandardTypeMap;
-			downs.session = db.Session;
 
-			downs.coltyp_map = new Dictionary<Column.Type, Type>();
-			foreach(var kv in downs.type_map)
-			{
-				try
-				{
-					downs.coltyp_map.Add(kv.Value, kv.Key);
-				}
-				catch(Exception)
-				{
-				}
-			}
-
-			var ups = Translator.Translate(exp, downs);
-
-			return new Query<T>(ups.plan, ups.chan, exp, this);
 		}
 
 		public IQueryable CreateQuery(Expression exp)
--- EseLinq/Query.cs
+++ EseLinq/Query.cs
@@ -4,33 +4,10 @@ using System.Collections;
 using System.Linq;
 using System.Linq.Expressions;
 
-using EseLinq.Plans;
 using EseObjects;
 
 namespace EseLinq
 {
-	using OperatorMap = OperatorMap;
-
-	internal interface QueryProperties
-	{
-		Plan plan
-		{
-			get;
-		}
-		Table table
-		{
-			get;
-		}
-		CalcPlan cplan
-		{
-			get;
-		}
-		Type type
-		{
-			get;
-		}
-	}
-
 	public class Query<T> : IQueryable<T>, IQueryable, IOrderedQueryable<T>, IOrderedQueryable, QueryProperties
 	{
 		Plan plan;
@@ -58,142 +35,14 @@ namespace EseLinq
 
 		public Query(Provider pro, Table table)
 		{
-			plan = new Scan(table);
-			var chan = new Translator.Channel(plan, table, typeof(T));
-			exp = MakeInject();
-			provider = pro;
-			this.calc = Plans.MakeObject.AutoCreate<T>(plan);
-			this.table = table;
-		}
-
-		internal Query(Plan plan, Plans.Translator.Channel chan, Expression exp, Provider pro)
-		{
-			this.plan = plan;
-			this.exp = exp;
-			this.provider = pro;
-			this.calc = chan.AsCalcPlan(typeof(T));
-			this.table = chan.table;
 		}
 
 		IEnumerator<T> IEnumerable<T>.GetEnumerator()
 		{
-			OperatorMap om = new OperatorMap();
-			
-			var op = om.Demand(plan);
-			var scalar = calc.ToCalc(om);
-
-			return new Executor(this, op, scalar);
 		}
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			OperatorMap om = new OperatorMap();
-
-			var op = plan.ToOperator(om);
-			var scalar = calc.ToCalc(om);
-
-			return new Executor(this, op, scalar);
-		}
-
-		internal class Executor : IEnumerator<T>
-		{
-			Query<T> query;
-			Operator top;
-			Calc value;
-
-			public Executor(Query<T> query, Operator top, Calc scalar)
-			{
-				this.query = query;
-				this.top = top;
-				this.value = scalar;
-			}
-
-			public T Current
-			{
-				get
-				{
-					return (T)value.value;
-				}
-			}
-
-			void IDisposable.Dispose()
-			{
-				top.Dispose();
-			}
-
-			object IEnumerator.Current
-			{
-				get
-				{
-					return this.Current;
-				}
-			}
-
-			bool IEnumerator.MoveNext()
-			{
-				return top.Advance();
-			}
-
-			void IEnumerator.Reset()
-			{
-				top.Reset();
-			}
-		}
-
-		public Type ElementType
-		{
-			get
-			{
-				return typeof(T);
-			}
-		}
-
-		public System.Linq.Expressions.Expression Expression
-		{
-			get
-			{
-				return exp;
-			}
-		}
-
-		public IQueryProvider Provider
-		{
-			get
-			{
-				return provider;
-			}
-		}
-
-		Plan QueryProperties.plan
-		{
-			get
-			{
-				return plan;
-			}
-		}
-
-		Table QueryProperties.table
-		{
-			get
-			{
-				return table;
-			}
-		}
-
-		CalcPlan QueryProperties.cplan
-		{
-			get
-			{
-				return calc;
-			}
-		}
-
-		Type QueryProperties.type
-		{
-			get
-			{
-				return typeof(T);
-			}
 		}
 	}
 }
